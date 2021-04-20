629a57bb85a253736548e710a55b94fdcb2f52db
Chris Chu <chrischu@thundercore.tw>
2021-04-20 17:54:27 +0800

    Add provider

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,24 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const privateKeys = Object.entries(process.env)
+  .filter(([key]) => key.startsWith('KEY'))
+  .map(([, secret]) => secret)
+
+const useProvider = (url) => () => {
+  if (!privateKeys.length) {
+    throw new Error('Require private keys to initialize provider')
+  }
+
+  return new HDWalletProvider(
+    privateKeys,
+    url,
+    0,
+    privateKeys.length
+  )
+}
+
 module.exports = {
+  plugins: ["truffle-plugin-save-per-network-deployment-record"],
   networks: {
     development: {
       host: "127.0.0.1", // Localhost (default: none)
@@ -6,11 +26,13 @@ module.exports = {
       network_id: "*", // Any network (default: none)
     },
     "thunder-testnet": {
+      provider: useProvider('https://testnet-rpc.thundercore.com'),
       network_id: "18",
       gas: 3000000,
       gasPrice: 1000000000,
     },
     "thunder-mainnet": {
+      provider: useProvider('https://mainnet-rpc.thundercore.com'),
       network_id: "108",
       gas: 3000000,
       gasPrice: 1000000000,
