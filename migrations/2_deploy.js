557660a6e113fee554946a6d180900fd69fdeca8
Chris Chu <chrischu@thundercore.tw>
2021-04-20 14:25:45 +0800

    Add example migration script to testnet and mainnet :zap:

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -3,19 +3,20 @@ const ERC20Token = artifacts.require('ERC20Token')
 const Factory = artifacts.require('AuctionFactory')
 
 module.exports = async function (deployer, network, [owner]) {
+  await Promise.all([
+    deployer.deploy(AuctionV1),
+    deployer.deploy(Factory),
+  ])
+
+  const [auction, factory] = await Promise.all([
+    AuctionV1.deployed(),
+    Factory.deployed(),
+  ])
+
   switch (network) {
     case 'development':
-      await Promise.all([
-        deployer.deploy(AuctionV1),
-        deployer.deploy(ERC20Token),
-        deployer.deploy(Factory),
-      ])
-
-      const [auction, token, factory] = await Promise.all([
-        AuctionV1.deployed(),
-        ERC20Token.deployed(),
-        Factory.deployed(),
-      ])
+      await deployer.deploy(ERC20Token)
+      const token = await ERC20Token.deployed()
 
       await factory.initialize()
       await factory.setTemplate(auction.address)
@@ -23,8 +24,16 @@ module.exports = async function (deployer, network, [owner]) {
 
       break
     case 'thunder-testnet':
+      await factory.initialize()
+      await factory.setTemplate(auction.address)
+      // TT-ETH address on ThunderCore Testnet
+      await factory.setTokenAddress('0x6757D10620c46219aF1E2f0E23144682d1aDBCC2')
+      break;
     case 'thunder-mainnet':
-      // Deploy to Thunder networks
+      await factory.initialize()
+      await factory.setTemplate(auction.address)
+      // TT-ETH address on ThunderCore Mainnet
+      await factory.setTokenAddress('0x6576Bb918709906DcbFDCeae4bB1e6df7C8a1077')
       break;
     default:
       throw new Error('Unsupported network')
